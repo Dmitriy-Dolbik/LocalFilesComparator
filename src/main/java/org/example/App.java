1e694242d91b0f44c89e481e4e85668759844931
ddolbik <d.dolbik@crystals.ru>
2023-11-12 11:42:50 +0300

    Refactoring

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,7 +6,7 @@ import org.example.contentCopiers.impl.FileCopier;
 public class App {
     public static void main(String[] args) {
         String parentFilePath = "/home/ddolbik/IdeaProjects/comparingLocalFiles/src/main/resources/res-visualisation-customer_be_BY.properties";
-        String childFilePath = "/home/ddolbik/IdeaProjects/comparingLocalFiles/src/main/resources/res-visualisation-scokiosk_be_BY.properties";
+        String childFilePath =  "/home/ddolbik/IdeaProjects/comparingLocalFiles/src/main/resources/res-visualisation-scokiosk_be_BY.properties";
 
         ContentCopier fileCopier = new FileCopier();
         String newDirectoryName = "/copies of the original files";
